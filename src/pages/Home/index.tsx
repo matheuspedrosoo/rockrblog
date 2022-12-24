284af4c0ac1725315ce92ff528c594bb922c9a56
Matheus Pedroso <matheuspedroso404@gmail.com>
2022-12-24 17:12:04 -0300

    feat: refatored load posts logic

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,17 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Header } from '../../components/Header/Header'
-
-import styles from './Home.module.scss'
-import React, { useEffect, useState } from 'react'
-import api from '../../api'
-import { IPostProps } from '../../interfaces/IPost'
+import React, { useEffect } from 'react'
 import { ScrollItens } from '../../components/ScrollItens'
 import { Loading } from '../../components/Loading'
+import { useFetch } from '../../hooks/useFetch'
+
+import styles from './Home.module.scss'
 
 function Home() {
-  const [posts, setPosts] = useState<IPostProps[]>([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [isFetching, setIsFetching] = useState(false)
-  let offset = 0
+  const { loadData, posts, isLoading, setIsLoading, isFetching, loadMorePosts } = useFetch()
 
   useEffect(() => {
     setIsLoading(true)
     loadData()
-    window.addEventListener('scroll', handleScroll)
   }, [])
 
   useEffect(() => {
@@ -25,47 +19,6 @@ function Home() {
       loadMorePosts()
     }
   }, [isFetching])
-
-  const loadData = () => {
-    setTimeout(async () => {
-      try {
-        const response = await api.get(`/posts?_limit=3$offset=${offset}`)
-        const { data } = response
-        setPosts(data)
-        setIsFetching(false)
-        setIsLoading(false)
-      } catch (error) {
-        console.log(error)
-      }
-    }, 1000)
-  }
-
-  const handleScroll = (e: any) => {
-    if (window.innerHeight + e.target.documentElement.scrollTop + 1 >= e.target.documentElement.scrollHeight) {
-      setIsFetching(true)
-    }
-  }
-
-  const loadMorePosts = () => {
-    setIsLoading(true)
-
-    setTimeout(async () => {
-      try {
-        const response = await api.get(`/posts?_limit=10&_sort=title&offset=${offset}`)
-        const { data } = response
-
-        const newPosts: IPostProps[] = []
-        data.forEach((item: IPostProps) => newPosts.push(item))
-
-        setPosts((oldState) => [...oldState, ...newPosts])
-        setIsFetching(false)
-
-        offset += 10
-      } catch (error) {
-        console.log(error)
-      }
-    }, 1000)
-  }
 
   return (
     <>
