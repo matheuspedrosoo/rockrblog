2a982aee51619d25f10455638076a97a7d7c842f
Matheus Pedroso <matheuspedroso404@gmail.com>
2022-12-24 19:36:56 -0300

    feat: implemented scrollTop when change route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Loading } from '../components/Loading'
+import { ScrollToTop } from '../components/ScrollTop'
 
 const Home = lazy(() => import('../pages/Home'))
 const Post = lazy(() => import('../pages/Post'))
@@ -8,6 +9,7 @@ const Post = lazy(() => import('../pages/Post'))
 export function Routers() {
   return (
     <Router>
+      <ScrollToTop />
       <Suspense fallback={<Loading />}>
         <Routes>
           <Route path="/" element={<Home />} />
